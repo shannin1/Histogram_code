da43db2fc0ec6aaccaed33b0ab6e462d963beabf
Sabrina Claire Hanning <shanning@lxplus989.cern.ch>
2024-06-14 14:55:15 +0200

    code for XHY_bbbb invariant mass plots

---
+++ gitdir/XY_mass_sum.py
@@ -0,0 +1,136 @@
+import numpy as np
+from lhereader import LHEReader
+import ROOT
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import mplhep as hep
+#from root_numpy import hist2array
+
+
+ROOT.gROOT.SetBatch(True)
+
+def MakeHistos(proc, vals):
+
+  lhef = os.path.join(os.getcwd(), vals['fin'])
+  fout = vals['fin'].split("/")[-1].replace('lhe', 'root')
+
+  fout = ROOT.TFile(fout, 'update')
+  fout.cd()
+
+  h_myh     = ROOT.TH1D('h_myh_{0}'.format(proc), ';m(#gamma, h) [GeV];Events;',100, 1399.99, 1400.01)
+  h_ctheta  = ROOT.TH1D('h_ctheta_{0}'.format(proc), ';cos(#theta);Events;',40, -1, 1)
+
+  reader = LHEReader(lhef)
+  
+  for iev, event in enumerate(reader):
+
+
+     # bs = list(filter(lambda x: abs(x.pdgid)== 5, event.particles))
+      print('events: ',event.particles)
+      
+      hs = list(filter(lambda x: abs(x.pdgid)== 25, event.particles))
+      ys = list(filter(lambda x: abs(x.pdgid)== 35, event.particles))
+      #print('hs:',hs)
+      #print('ys:',ys)
+      if(len(hs)!=1 or len(ys)!=1):
+        print("WARNING: More than one H/y in event")
+
+      #print('hvalue: ',hs[0].p4().mass)
+      #print('yvalue: ',ys[0].p4().mass)
+      h_myh.Fill((hs[0].p4() + ys[0].p4()).mass )
+      h_p    = np.sqrt(hs[0].energy**2-hs[0].mass**2)
+      ctheta = hs[0].pz/h_p
+
+      h_ctheta.Fill(ctheta)
+
+  fout.Write()
+  fout.Close()
+
+def MakePlot(procs,log=True,ofile="test.png"):
+  
+  f       = ROOT.TFile.Open("cmsgrid_final.root")
+  
+  histos  = []
+  edges   = []
+  colors  = []
+  labels  = []
+
+  for proc in procs:
+    h     = f.Get('h_myh_{0}'.format(proc))
+    #print('h: ',h)
+    #print('int: ',h.Integral())
+    #h.Scale(1./h.Integral())
+    #hist, edges_2D = hist2array(h,return_edges=True)
+    #edges = edges_2D[0]
+    
+    #edges = [h.GetXaxis().GetBinLowEdge(i) for i in range(1, h.GetNbinsX() + 1)]
+    #edges.append(h.GetXaxis().GetBinUpEdge(h.GetNbinsX()))
+    #hist = np.array([h.GetBinContent(i) for i in range(1, h.GetNbinsX() + 1)])
+
+    nbins = h.GetNbinsX()
+    edges = [h.GetBinLowEdge(i) for i in range(1, nbins + 2)]  # Include overflow bin
+    hist = np.array([h.GetBinContent(i) for i in range(1, nbins + 1)])  # Exclude overflow bin
+
+
+    print(edges)
+    print(hist)
+
+    histos.append(hist)
+    colors.append(procs[proc]["color"])
+    labels.append(procs[proc]["label"])
+
+  plt.style.use([hep.style.CMS])
+  f, ax   = plt.subplots()
+  plt.sca(ax)
+  hep.histplot(histos,edges,stack=False,ax=ax,label=labels,linewidth=2,histtype="step",edgecolor=colors)
+  if(log):
+    ax.set_yscale("log")
+    ax.set_ylim([0.01,10**0])
+  else:
+    ax.set_ylim([0.,30])
+  ax.set_xlim([1399.99, 1400.01])
+  plt.xlabel("$m_{H,Y}$",horizontalalignment='left', x=1.0)
+  plt.ylabel("Events / bin A.U."  ,horizontalalignment='right', y=1.0)
+
+  hep.cms.lumitext(text="(13 TeV)", ax=ax)
+  plt.legend(loc="best")
+  
+  plt.tight_layout()
+  print(ofile)
+  plt.savefig(ofile)
+  plt.show()
+
+def main():
+
+  procs = {
+      'HZy': {
+        'fin': "cmsgrid_final.lhe",
+        'color':"red",
+        'label':"HY"
+        },
+
+      #'Hyy': {
+       # 'fin': "Hyy/cmsgrid_final.lhe",
+        #'color':"darkgreen",
+        #'label':"JHUGen H$\gamma\gamma$"
+     #   },
+      #'SM_LO': {
+       # 'fin': "SM_LO/cmsgrid_final.lhe",
+        #'color':"blue",
+        #'label':"MadGraph LO"
+        #},
+
+      }
+
+  for proc, vals in procs.items():
+    MakeHistos(proc, vals)
+
+  #MakePlot(procs,log=True,ofile="myh_log.png")
+  MakePlot(procs,log=False,ofile="myh_lin.png")
+  #cwd = os.getcwd()
+  #print("Current working directory:", cwd)
+
+if __name__ == "__main__":
+  main()
---
+++ gitdir/bbbar_same_parent.py
@@ -0,0 +1,155 @@
+import numpy as np
+from lhereader import LHEReader
+import ROOT
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import mplhep as hep
+#from root_numpy import hist2array
+
+
+ROOT.gROOT.SetBatch(True)
+
+def MakeHistos(proc, vals):
+
+  lhef = os.path.join(os.getcwd(), vals['fin'])
+  fout = vals['fin'].split("/")[-1].replace('lhe', 'root')
+
+  fout = ROOT.TFile(fout, 'update')
+  fout.cd()
+
+  h_myh     = ROOT.TH1D('h_myh_{0}'.format(proc), ';m(#gamma, h) [GeV];Events;',100, 124.96,125.06)#4.21398*2, 4.21341*2)
+  h_ctheta  = ROOT.TH1D('h_ctheta_{0}'.format(proc), ';cos(#theta);Events;',40, -1, 1)
+  
+
+  reader = LHEReader(lhef)
+  
+  bin=0
+
+  for iev, event in enumerate(reader):
+
+
+     # bs = list(filter(lambda x: abs(x.pdgid)== 5, event.particles))
+      #print(event.particles)
+      
+      #PARENTS ARE 3 AND 4
+      bs = list(filter(lambda x: x.pdgid== 5, event.particles))
+      bs = list(filter(lambda x: abs(x.parent)== 3, bs))
+      bbars = list(filter(lambda x: x.pdgid== -5, event.particles))
+      bbars = list(filter(lambda x: abs(x.parent)== 3, bbars))
+
+      #print('bs:',bs)
+      
+      """if(len(hs)!=1 or len(ys)!=1):
+        print("WARNING: More than one H/y in event")
+      """
+      #print('hvalue: ',hs[0].p4().mass)
+      #print('yvalue: ',ys[0].p4().mass)
+      #print(bs[0].p4().mass)
+      h_myh.Fill((bs[0].p4() + bbars[0].p4()).mass )
+      
+      #print('sum',(bs[0].p4() + bbars[0].p4()).mass )
+      sum=(bs[0].p4() + bbars[0].p4()).mass
+      print(sum)
+      if (sum>124.96) and (sum<125.06):
+        bin+=1
+      
+      #h_p    = np.sqrt(bs[0].energy**2-bs[0].mass**2)
+      #ctheta = bs[0].pz/h_p
+      
+
+      #h_ctheta.Fill(ctheta)
+  print('bin',bin)
+
+  fout.Write()
+  fout.Close()
+
+def MakePlot(procs,log=True,ofile="test.png"):
+  
+  f       = ROOT.TFile.Open("cmsgrid_final.root")
+  
+  histos  = []
+  edges   = []
+  colors  = []
+  labels  = []
+
+  for proc in procs:
+    h     = f.Get('h_myh_{0}'.format(proc))
+    #print('h: ',h)
+    #print('int: ',h.Integral())
+    #h.Scale(1./h.Integral())
+    #hist, edges_2D = hist2array(h,return_edges=True)
+    #edges = edges_2D[0]
+    
+    #edges = [h.GetXaxis().GetBinLowEdge(i) for i in range(1, h.GetNbinsX() + 1)]
+    #edges.append(h.GetXaxis().GetBinUpEdge(h.GetNbinsX()))
+    #hist = np.array([h.GetBinContent(i) for i in range(1, h.GetNbinsX() + 1)])
+
+    nbins = h.GetNbinsX()
+    print(nbins)
+    edges = [h.GetBinLowEdge(i) for i in range(1, nbins + 2)]  # Include overflow bin
+    hist = np.array([h.GetBinContent(i) for i in range(1, nbins + 1)])  # Exclude overflow bin
+
+
+    print(edges)
+    print(hist)
+
+    histos.append(hist)
+    colors.append(procs[proc]["color"])
+    labels.append(procs[proc]["label"])
+
+  plt.style.use([hep.style.CMS])
+  f, ax   = plt.subplots()
+  plt.sca(ax)
+  hep.histplot(histos,edges,stack=False,ax=ax,label=labels,linewidth=2,histtype="step",edgecolor=colors)
+  if(log):
+    ax.set_yscale("log")
+    ax.set_ylim([0.01,10**0])
+  else:
+    ax.set_ylim([0.,50])
+  ax.set_xlim([124.96,125.06])
+  #ax.set_xlim([4.21398*2, 4.21341*2])
+  plt.xlabel("$m_{b,b~}$",horizontalalignment='left', x=1.0)
+  plt.ylabel("Events / bin A.U."  ,horizontalalignment='right', y=1.0)
+
+  hep.cms.lumitext(text="(13 TeV)", ax=ax)
+  plt.legend(loc="best")
+  
+  plt.tight_layout()
+  print(ofile)
+  plt.savefig(ofile)
+  plt.show()
+
+def main():
+
+  procs = {
+      'HZy': {
+        'fin': "cmsgrid_final.lhe",
+        'color':"red",
+        'label':"bbar" #"JHUGen HZ$\gamma$"
+        },
+
+      #'Hyy': {
+       # 'fin': "Hyy/cmsgrid_final.lhe",
+        #'color':"darkgreen",
+        #'label':"JHUGen H$\gamma\gamma$"
+     #   },
+      #'SM_LO': {
+       # 'fin': "SM_LO/cmsgrid_final.lhe",
+        #'color':"blue",
+        #'label':"MadGraph LO"
+        #},
+
+      }
+
+  for proc, vals in procs.items():
+    MakeHistos(proc, vals)
+
+  #MakePlot(procs,log=True,ofile="bbar_parent_3_log.png")
+  MakePlot(procs,log=False,ofile="bbar_parent_3.png")
+  #cwd = os.getcwd()
+  #print("Current working directory:", cwd)
+
+if __name__ == "__main__":
+  main()
